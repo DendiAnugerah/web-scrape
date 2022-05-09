12111212d4211087cfbd1ec19c3376ebe23c8294
DendiAnugerah <dendiaa@students.amikom.ac.id>
2022-05-09 14:40:36 +0700

    F commit

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	"net/htpp"
+	"net/http"
+	"os"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -14,12 +17,18 @@ func check(err error){
 	}
 }
 
+func WriteFile(data, filename string){
+	file, err := os.Create(filename)
+	defer file.Close()
+	check(err)
 
+	file.WriteString(data)
+}
 
 func main(){
 	url := "https://techcrunch.com/"
 
-	response, err := htpp.Get(url)
+	response, err := http.Get(url)
 	defer response.Body.Close()
 	check(err)
 
@@ -27,11 +36,23 @@ func main(){
 		fmt.Println("Status code: ", response.StatusCode)
 	}
 
-	doc, err := goquery.NewDocument(response.Body)
-	check(err)
-	river, err := doc.Find("div.river").Html()
+	doc, err := goquery.NewDocumentFromReader(response.Body)
 	check(err)
 
-	fmt.Println(river)
+	file, err := os.Create("posts.csv")
+	check(err)
+
+	writer := csv.NewWriter(file)
+	doc.Find("div.river").Find("div.post-block ").Each(func (index int, item *goquery.Selection)  {
+		h2 := item.Find("h2")
+		title := strings.TrimSpace(h2.Text())
+		url, _ := h2.Find("a").Attr("href")
+
+		excerpt := strings.TrimSpace(item.Find("div.post-bloc__content").Text())
+		posts := []string{title, url, excerpt}
+
+		writer.Write(posts)
+	})
+	writer.Flush()
 
 }
